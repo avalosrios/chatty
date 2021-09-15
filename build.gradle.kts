9001c712fb8910f787ebb7a58ea347742d3270ab
Oswaldo Avalos <avalosrios@gmail.com>
2021-09-15 14:49:08 -0700

    Initial commit
    - Sets project file, gradle config, spring boot
    - Adds dgs libraries for GRAPHQL
    - Add graphql files with the initial graphql api definitions

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.5.4"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.5.21"
+    kotlin("plugin.spring") version "1.5.21"
+    kotlin("plugin.jpa") version "1.5.21"
+    kotlin("plugin.allopen") version "1.5.21"
+    kotlin("plugin.noarg") version "1.5.21"
+    id("com.netflix.dgs.codegen") version "5.0.6"
+}
+
+group = "com.avalos"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_16
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // Spring Boot's JPA support
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // Graphql stuff
+    implementation("com.graphql-java:graphql-java-extended-scalars:16.0.1")
+    implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:latest.release"))
+    implementation("com.netflix.graphql.dgs:graphql-dgs-spring-boot-starter")
+    implementation("com.netflix.graphql.dgs:graphql-dgs-pagination")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("com.h2database:h2")
+
+    // Testing deps
+    testImplementation("com.ninja-squad:springmockk:3.0.1") {
+        exclude(module = "mockito-core")
+    }
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(group = "com.h2database", name = "h2")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "16"
+    }
+}
+
+tasks.withType<com.netflix.graphql.dgs.codegen.gradle.GenerateJavaTask> {
+    generateClient = true
+    packageName = "com.petme.petmeapi.generated"
+    language = "kotlin"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
