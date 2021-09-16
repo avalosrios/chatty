629f9ff44afb1bc6ec842eba0ef8188e432f423a
Oswaldo Avalos <avalosrios@gmail.com>
2021-09-15 18:51:27 -0700

    Adds Message Service
    - Adds basic implementation for message service
    - Adds service into data fetcher

--- src/main/kotlin/com/avalos/chatty/datafetchers/MessageDataFetcher.kt
+++ src/main/kotlin/com/avalos/chatty/datafetchers/MessageDataFetcher.kt
@@ -2,22 +2,19 @@ package com.avalos.chatty.datafetchers
 
 import com.avalos.chatty.generated.DgsConstants
 import com.avalos.chatty.generated.types.Message
+import com.avalos.chatty.services.MessageService
 import com.netflix.graphql.dgs.DgsComponent
 import com.netflix.graphql.dgs.DgsQuery
 import graphql.relay.*
 import graphql.schema.DataFetchingEnvironment
 
 @DgsComponent
-class MessageDataFetcher {
+class MessageDataFetcher(private val messageService: MessageService) {
     @DgsQuery(field = DgsConstants.QUERY.Messages)
     fun messages(dfe: DataFetchingEnvironment): Connection<Message> {
         val first = dfe.arguments["first"] as Int? ?: 10
         val after = dfe.arguments["after"] as Int? ?: 0
-        val messages = listOf(
-            Message(id = "1", text = "test"),
-            Message(id = "2", text = "b"),
-            Message(id = "3", text = "c")
-        )
+        val messages = messageService.getMessages(first, after)
         val edges = messages.mapIndexed { idx, msg -> DefaultEdge(msg, DefaultConnectionCursor(idx.toString())) }
         val hasNextPage = true // TODO: Make sure we calculate this one
         return DefaultConnection(
--- src/test/kotlin/com/avalos/chatty/datafetchers/MessageMutationTest.kt
+++ src/test/kotlin/com/avalos/chatty/datafetchers/MessageMutationTest.kt
@@ -4,14 +4,19 @@ import com.avalos.chatty.generated.client.AddMessageGraphQLQuery
 import com.avalos.chatty.generated.client.AddMessageProjectionRoot
 import com.avalos.chatty.generated.types.AddMessageData
 import com.avalos.chatty.generated.types.Message
+import com.avalos.chatty.services.MessageService
 import com.netflix.graphql.dgs.DgsQueryExecutor
 import com.netflix.graphql.dgs.autoconfig.DgsAutoConfiguration
 import com.netflix.graphql.dgs.client.GraphQLResponse
 import com.netflix.graphql.dgs.client.codegen.GraphQLQueryRequest
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import io.mockk.verify
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -20,6 +25,15 @@ internal class MessageMutationTest {
 
     @Autowired
     lateinit var dgsQueryExecutor: DgsQueryExecutor
+
+    @MockkBean
+    lateinit var messageService: MessageService
+
+    @BeforeEach
+    fun beforeEach() {
+        val mockMessage = Message(id = "1", text = "test")
+        every { messageService.create(any()) } returns mockMessage
+    }
 
     @Test
     fun addMessage() {
@@ -39,5 +53,7 @@ internal class MessageMutationTest {
         val response = GraphQLResponse(context.jsonString())
         val text = response.extractValue<String>("data.addMessage.text")
         Assertions.assertThat(text).contains("test")
+
+        verify { messageService.create(any()) }
     }
 }
