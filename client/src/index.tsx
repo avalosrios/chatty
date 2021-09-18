b43c22aac96cc201a6761db92f03fa4f79fffb6a
Oswaldo Avalos <avalosrios@gmail.com>
2021-09-17 18:17:18 -0700

    Adds Grommet for UI

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,6 @@
 import {ApolloProvider} from "@apollo/client";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {client} from './shared/apollo-client';
