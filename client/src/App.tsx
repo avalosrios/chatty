0af31470851c5ab0472ad2b98072dd1f27c54c43
Oswaldo Avalos <avalosrios@users.noreply.github.com>
2021-09-17 18:17:45 -0700

    Adds Grommet for UI (#10)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,24 @@
+import {Box, Grommet} from 'grommet';
 import React from 'react';
 import './App.css';
 import {Home} from './components/Home';
 
 const App = () => {
+  const theme = {
+    global: {
+      font: {
+        family: 'Roboto',
+        size: '18px',
+        height: '20px',
+      },
+    },
+  };
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Grommet theme={theme} full>
+      <Box fill>
+        <Home/>
+      </Box>
+    </Grommet>
   );
 }
 
